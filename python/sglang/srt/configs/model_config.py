dafee8a99a35750db2b7f54e82176c44737c4ad2
Mengni Wang <mengni.wang@intel.com>
2026-01-26 13:50:43 +0000

    update
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -798,6 +798,7 @@ class ModelConfig:
             "mxfp4",
             "auto-round",
             "quark_int4fp8_moe",
+            "auto-round-int8",
         ]
         optimized_quantization_methods = [
             "fp8",
@@ -819,7 +820,6 @@ class ModelConfig:
             "petit_nvfp4",
             "quark",
             "modelslim",
-            "autoround_w4a16",
         ]
         compatible_quantization_methods = {
             "modelopt_fp8": ["modelopt"],
